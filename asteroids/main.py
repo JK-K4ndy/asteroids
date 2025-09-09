18474f01819ca8631b895a4f0c1daf927f949898
K4ndy <JakubKandler@gmail.com>
2025-09-09 23:36:46 +0200

    Pause handling

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame # type: ignore
 
 from constants import *
 
@@ -7,16 +7,38 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    running = True
+    paused = False
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,16))
-        pygame.display.flip()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                paused = not paused #toggles pause when "P" is pressed
+                if paused:
+                    print("GAME PAUSED")
+                    continue
+                else:
+                    print("GAME UNPAUSED")
+                    continue
+            #TODO assign more control
+
+        #Pause handling
+        if paused:
+            screen.fill((20, 20, 40))
+            pygame.display.flip()
+            dt = clock.tick(60) / 1000
+            continue
         
-
-
+        #
+        screen.fill((0,0,16))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
