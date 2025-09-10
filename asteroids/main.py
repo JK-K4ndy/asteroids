d6ad12fa91a0b818b67caa23f140d9b27f5804d3
K4ndy <JakubKandler@gmail.com>
2025-09-10 20:24:03 +0200

    Added player with WSAD control

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,7 @@
 import pygame # type: ignore
 
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -14,6 +15,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -37,6 +44,10 @@ def main():
         
         #
         screen.fill((0,0,16))
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
